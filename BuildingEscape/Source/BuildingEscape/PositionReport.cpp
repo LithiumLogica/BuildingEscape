103129a2735b4e25cc24e4021063c96b9e215dcf
Lithium Logica <lithiumlogica@gmail.com>
2018-11-04 21:23:42 -0700

    After cleaning up weird error with some xcsvj or whatever file.

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BuildingEscape.h"
 #include "PositionReport.h"
+#include "BuildingEscape.h"
 
 
 // Sets default values for this component's properties
